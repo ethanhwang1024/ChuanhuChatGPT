261f73be77de60f921b89b7084775c9a2e8b9146
ethanhwang1024 <89822193+ethanhwang1024@users.noreply.github.com>
2023-03-31 00:22:43 +0900

    up

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -17,7 +17,7 @@ logging.basicConfig(
     format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s",
 )
 
-my_api_key = "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"  # 在这里输入你的 API 密钥
+my_api_key = "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"  # 在这里输入你的 API 密钥
 
 # if we are running in Docker
 if os.environ.get("dockerrun") == "yes":
